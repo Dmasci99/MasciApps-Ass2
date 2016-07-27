04217b06533b7fb0c79e4608150e601bd5d12047
Dan Masci <minimasci@yahoo.ca>
2016-07-26 20:38:23 -0400

    Add project files.

---
+++ MasciApps-Ass2/Controllers/StoreController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MasciApps_Ass2.Models;
+
+namespace MasciApps_Ass2.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities(); //Gain access to all our Table Info
+
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            List<Genre> genres = storeDB.Genres.ToList();
+            return View(genres);
+        }
+        //
+        // GET: /Store/Browse?genre=jazz
+        public ActionResult Browse(string genre = "Rock")
+        {
+            // Retrieve Genre and its Associated Albums from database
+            Genre genreModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
+            return View(genreModel);
+        }
+        //
+        // GET: /Store/Details
+        public ActionResult Details(int id = 1)
+        {
+            Album album = storeDB.Albums.Find(id);
+            return View(album);
+        }
+    }
+}
