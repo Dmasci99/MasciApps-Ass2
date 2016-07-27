383708958938f1df425c679b6c8da23827fccfd1
Dan Masci <minimasci@yahoo.ca>
2016-07-27 15:25:50 -0400

    Home and Contact Pages -Home page slider fixed and new images swapped -Contact page redone with map and directions -Dashboard background image added as global interior-header background-image

--- MasciApps-Ass2/Controllers/HomeController.cs
+++ MasciApps-Ass2/Controllers/HomeController.cs
@@ -15,15 +15,12 @@ namespace MasciApps_Ass2.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Title = "Contact";
             return View();
         }
     }
